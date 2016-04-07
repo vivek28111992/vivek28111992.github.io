6a43aaf4fccfabff47a780778a6f43425e6a3d50
Vivek Pawar <pawarvivek29@gmail.com>
2016-04-07 08:08:18 +0530

    about commit

--- js/myjs.js
+++ js/myjs.js
@@ -4,6 +4,7 @@
       $(".element").typed({
         strings: ["is Web Developer.", "like js.", "love football."],
         loop: true,
+        backDelay: 1000,
         typeSpeed: 0
       });
   });
